26b5a69ce233d8e9bb60bfd524270d1e0f58ea89
Benjamin McIntyre <benjaminmcintyre06@protonmail.com>
2025-09-17 16:43:19 -0400

    Add ben's intro to 1st gbm

---
+++ 25-26/1stGBM/BenIntro.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+trap read debug
+printf "\e]66;s=4;Benjamin P. McIntyre\a\n\n\n\n\e"
+printf "\e]66;s=2;Major: Computer Engineering\a\n\n\n\r\e"
+printf "\e]66;s=2;Years on Linux: 5\a\n\n\r\e"
+fastfetch
+printf "\e]66;s=2;Hobbies: Ricing, Food [fishing, foraging, fermenting, gardening.], Urban Exploration\a\n\n\r\e"
